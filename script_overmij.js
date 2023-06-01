6432ea22ac277722cbd2270a454889aba6ce7eef
thomaspeeterspxl <thomas.peeters3@student.pxl.be>
2023-06-02 00:33:13 +0200

    test

---
+++ script_overmij.js
@@ -0,0 +1,9 @@
+document.getElementsByTagName("button")[0].addEventListener('mouseover', changecolor);
+document.getElementsByTagName("button")[0].addEventListener('mouseout', resetcolor);
+
+function changecolor(){
+    document.getElementsByTagName("button")[0].style.background = "#545479"
+}
+function resetcolor(){
+    document.getElementsByTagName("button")[0].style.background = "#5C9D8B"
+}
