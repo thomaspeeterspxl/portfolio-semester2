ad43c0fa7f1762cb640a7de08a835dc45affa986
thomaspeeterspxl <thomas.peeters3@student.pxl.be>
2023-06-02 12:36:28 +0200

    test

--- script_overmij.js
+++ script_overmij.js
@@ -3,6 +3,7 @@ document.getElementsByTagName("button")[0].addEventListener('mouseout', resetcol
 
 function changecolor(){
     document.getElementsByTagName("button")[0].style.background = "#545479"
+    document.getElementsByTagName("button")[0].style.border = "#545479"
 }
 function resetcolor(){
     document.getElementsByTagName("button")[0].style.background = "#5C9D8B"
